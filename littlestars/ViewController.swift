b7b42d22ce493a7ef594b841dcdc83d4860c118f
llanterme <Luke@Lukes-MacBook-Pro.local>
2015-01-09 12:17:07 +0200

    Initial Commit

---
+++ littlestars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  littlestars
+//
+//  Created by Luke Lanterme on 2015/01/09.
+//  Copyright (c) 2015 Luke Lanterme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
